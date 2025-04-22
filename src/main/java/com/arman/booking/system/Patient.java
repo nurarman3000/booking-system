56702e7ad4170874d752d4af984e8417e76a43b9
Nur Mohammad Arman <nurmohammad.arman97@gmail.com>
2025-04-22 23:43:37 +0200

    Patient inherited Member

---
+++ src/main/java/com/arman/booking/system/Patient.java
@@ -0,0 +1,11 @@
+package com.arman.booking.system;
+
+/**
+ *
+ * @author NurArman
+ */
+class Patient extends Member {
+    public Patient(String id, String fullName, String address, String phoneNumber) {
+        super(id, fullName, address, phoneNumber);
+    }
+}
