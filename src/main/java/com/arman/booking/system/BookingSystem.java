b6cbc3d43d529941c728fea0f69980cd2383c770
Nur Mohammad Arman <nurmohammad.arman97@gmail.com>
2025-04-22 23:22:01 +0200

    init commit

---
+++ src/main/java/com/arman/booking/system/BookingSystem.java
@@ -0,0 +1,9 @@
+package com.arman.booking.system;
+
+public class BookingSystem {
+    public static void main(String[] args) {
+      
+        System.out.println("Welcome to Boost Physio Clinic (BPC) Booking System!");
+
+    }
+}
