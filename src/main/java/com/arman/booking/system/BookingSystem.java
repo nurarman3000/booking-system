f8e199a9477fb0b767f33af405884ba965cc17d6
Nur Mohammad Arman <nurmohammad.arman97@gmail.com>
2025-04-24 01:07:42 +0200

    Taking input from user to selectadd patient function of the tool and show menu to the user

--- src/main/java/com/arman/booking/system/BookingSystem.java
+++ src/main/java/com/arman/booking/system/BookingSystem.java
@@ -1,9 +1,43 @@
 package com.arman.booking.system;
 
+import java.util.Scanner;
+
 public class BookingSystem {
+
+    private static Clinic clinic;
+    private static Scanner scanner;
+
     public static void main(String[] args) {
-      
+        clinic = DataInitializer.initializeClinic(); // Initialize clinic with preloaded data
+        scanner = new Scanner(System.in);
+
         System.out.println("Welcome to Boost Physio Clinic (BPC) Booking System!");
 
+        boolean isRunning = true;
+        while (isRunning) {
+            showMenu();
+            String userInput = scanner.nextLine();
+            switch (userInput.toLowerCase()) {
+                case "1":
+                    ClinicUtil.addPatient(clinic);
+                    break;
+                case "exit":
+                    isRunning = false;
+                    break;
+                default:
+                    System.out.println("Invalid option! Please try again.");
+                    break;
+            }
+        }
+
+        System.out.println("Exiting... Thank you for using BPC Booking System!");
+        scanner.close();
+    }
+
+    private static void showMenu() {
+        System.out.println("\n--- Main Menu ---");
+        System.out.println("1. Add a new patient");
+        System.out.println("Type 'exit' to quit");
+        System.out.print("Choose an option: ");
     }
 }
