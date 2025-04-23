942307b130ec6dab556a1844e27f9d961e8b735c
Nur Mohammad Arman <nurmohammad.arman97@gmail.com>
2025-04-23 20:04:51 +0200

    Appointment functionality added

--- src/main/java/com/arman/booking/system/Appointment.java
+++ src/main/java/com/arman/booking/system/Appointment.java
@@ -19,4 +19,27 @@ public class Appointment {
         this.treatment = treatment;
         this.status = AppointmentStatus.BOOKED; // Temporary until booked
     }
+
+    public void book(Patient patient) {
+        this.patient = patient;
+        this.status = AppointmentStatus.BOOKED;
+    }
+
+    public void cancel() {
+        this.status = AppointmentStatus.CANCELLED;
+    }
+
+    public void attend() {
+        this.status = AppointmentStatus.ATTENDED;
+    }
+
+    public boolean isAvailable() {
+        return this.patient == null && this.status != AppointmentStatus.CANCELLED;
+    }
+
+    public LocalDateTime getStartTime() { return startTime; }
+    public LocalDateTime getEndTime() { return endTime; }
+    public Treatment getTreatment() { return treatment; }
+    public Patient getPatient() { return patient; }
+    public AppointmentStatus getStatus() { return status; }
 }
