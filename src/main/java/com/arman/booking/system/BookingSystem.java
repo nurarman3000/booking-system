4b536c38068296b3ece27f3f630f86421b4cf48a
Nur Mohammad Arman <nurmohammad.arman97@gmail.com>
2025-04-24 18:32:49 +0200

    Added menu and it's corresponding items

--- src/main/java/com/arman/booking/system/BookingSystem.java
+++ src/main/java/com/arman/booking/system/BookingSystem.java
@@ -22,7 +22,24 @@ public class BookingSystem {
                     ClinicUtil.addPatient(clinic);
                     break;
                 case "2":
+                    ClinicUtil.searchByExpertise(clinic);
+                    break;
+                case "3":
                     ClinicUtil.searchByPhysiotherapist(clinic);
+                    break;
+                case "4":
+                    ClinicUtil.bookAppointment(clinic);
+                    break;
+                case "5":
+                    ClinicUtil.cancelAppointment(clinic);
+                    break;
+                case "6": 
+                    ClinicUtil.attendAppointment(clinic);
+                case "7":
+                    ClinicUtil.generateReport(clinic);
+                    break;
+                case "8":
+                    ClinicUtil.listAllPatients(clinic);
                     break;
                 case "exit":
                     isRunning = false;
@@ -41,6 +58,12 @@ public class BookingSystem {
         System.out.println("\n--- Main Menu ---");
         System.out.println("1. Add a new patient");
         System.out.println("2. Search by expertise");
+        System.out.println("3. Search by physiotherapist");
+        System.out.println("4. Book an appointment");
+        System.out.println("5. Cancel an appointment");
+        System.out.println("6. Generate report");
+        System.out.println("7. See Patient List");
+        System.out.println("Type 'exit' to quit");
         System.out.print("Choose an option: ");
     }
 }
